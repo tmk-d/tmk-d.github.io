5910de37d651154917697db835296a503a29602b
tmk-d <tomek.detlaf@gmail.com>
2018-05-19 21:37:44 +0200

    → Bootstrap → v.2
    → dodano dwie karty
     Changes to be committed:
    	zmodyfikowany: hs/index.html
    	zmodyfikowany: hs/script.js
    	nowy plik:     hs/select.html

--- hs/script.js
+++ hs/script.js
@@ -1,20 +1,56 @@
-function init1(){
-	corslink="https://cors.io/?";
-	sheetlink="http://gsx2json.com/api?id=1z9hG-qSjrLTNY37crkBgIVt3IpjmVFYrDHeTsFqQHi4";
-	x=$.getJSON(corslink+sheetlink, function(data) {});
+function init(){
+	link="https://spreadsheets.google.com/feeds/list/1z9hG-qSjrLTNY37crkBgIVt3IpjmVFYrDHeTsFqQHi4/od6/public/values?alt=json";
+	$.getJSON(link, function(data) {
+		T=gsx2json(data);
+		btn.disabled=false;
+		$("table")[0].appendChild(maketable())
+		});
 	points=0;
 	count=0;
 	succ=0;
 	}
 
-function init2(){
-	T=x.responseJSON;	
-	next();
+function obj2td(obj){
+	var td = document.createElement("td");
+	td.innerHTML=obj;
+	return td;
+	}
+
+function maketable(){
+	var frag = document.createDocumentFragment();
+	for(i in T){
+		var tr = document.createElement("tr");
+		tr.appendChild(obj2td(T[i]["tytuł"]));
+		tr.appendChild(obj2td(T[i]["autor"]));
+		tr.appendChild(obj2td(T[i]["nurt"]));
+		tr.appendChild(obj2td(
+			'<a href="'+T[i].link+'">Link</a>'
+		));
+		frag.appendChild(tr);
+		}
+	var tb = document.createElement("tbody");
+	tb.appendChild(frag);
+	return tb;
 	}
 	
+function gsx2json(gsdata){
+	gsdata=gsdata.feed.entry;
+	var table=[];
+	for (i in gsdata){
+		var row={};
+		row["tytuł"]=gsdata[i].gsx$tytuł.$t;
+		row["autor"]=gsdata[i].gsx$autor.$t;
+		row["nurt"]=gsdata[i].gsx$nurt.$t;
+		row["link"]=gsdata[i].gsx$link.$t;
+		table[i]=row;
+		}
+	return table;
+	}
+
+
 function singlecheck(o, i){
 	if (i.value==o.textContent){
-		i.style.backgroundColor="--green";
+		i.style.backgroundColor="#28a745";
 		points++;
 		succtmp++;
 		}
@@ -25,20 +61,18 @@ function singlecheck(o, i){
 	}
 	
 function check(){
-	tytO.textContent=T.columns["tytuł"][num];
+	tytO.textContent=T[num]["tytuł"];
 	singlecheck(tytO,tytI);
 	//T.columns["tytuł"].splice(num,1);
 	
-	autO.textContent=T.columns["autor"][num];
+	autO.textContent=T[num]["autor"];
 	singlecheck(autO,autI);
 	//T.columns["autor"].splice(num,1);
 	
-	nurO.textContent=T.columns["nurt"][num];
+	nurO.textContent=T[num]["nurt"];
 	singlecheck(nurO,nurI);
 	//T.columns["nurt"].splice(num,1);
 	
-	//T.rows.splice(num,1);
-
 	pkt.textContent=points+"/"+(3*count);
 	
 	if(succtmp==3)
@@ -52,11 +86,11 @@ function check(){
 	btn.onclick=function(){next()};
 	}
 function next(){
-	//if (T.rows.length==0)
+	//if (T.length==0)
 	//	alert("koniec");
 	
-	num=Math.floor(Math.random()*T.rows.length);
-	obr.src=T.columns["link"][num];
+	num=Math.floor(Math.random()*T.length);
+	obr.src=T[num]["link"];
 	//T.columns["link"].splice(num,1);
 	
 	tytO.textContent="";
